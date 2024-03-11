ae6fec09e44eed66943b03af6025d17d4f4d1d6f
Hussein380 <huznigarane@gmail.com>
2024-03-11 11:49:35 +0300

    corrected the docstring documenttion to follow PEP8

--- pycodestyle.py
+++ pycodestyle.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def swap_numbers(a, b):
-    """swap integer a and b"""
+    """
+    swap integer a and b
+    :wq
+    a: the first integer to be swappd
+    b: the second integer to be swappedi
+
+    """
     temp = a
     a = b
     b = temp
